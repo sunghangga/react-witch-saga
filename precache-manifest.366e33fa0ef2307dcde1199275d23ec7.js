2510199117eeb6ee286ddcf02d91634e1de1d672
Angga Pramana Putra <sunghangga@gmail.com>
2022-08-12 00:54:09 +0800

    Updates

---
+++ precache-manifest.366e33fa0ef2307dcde1199275d23ec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da5e583b1fb81b3c9b8e08e1359ff77",
+    "url": "/react-witch-saga/index.html"
+  },
+  {
+    "revision": "7f46e5345f8ddb5627fe",
+    "url": "/react-witch-saga/static/css/main.08a69aca.chunk.css"
+  },
+  {
+    "revision": "2a1ea72b2830067c834a",
+    "url": "/react-witch-saga/static/js/2.6e56dc5b.chunk.js"
+  },
+  {
+    "revision": "7f46e5345f8ddb5627fe",
+    "url": "/react-witch-saga/static/js/main.0e73e57f.chunk.js"
+  },
+  {
+    "revision": "66a4e2a327cf14521411",
+    "url": "/react-witch-saga/static/js/runtime~main.0f962669.js"
+  }
+]);
